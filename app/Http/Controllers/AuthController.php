1ad0521d742f8071afea1004a3df95c04c47dd0a
010Maximus010 <muh.afiatyulianto123@gmail.com>
2024-04-19 13:00:10 +0700

    test uplod

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,7 +41,7 @@ class AuthController extends Controller
             return redirect()->intended('dashboard');
         }
 
-        return back()->withErrors([
+        return back()->with([
             'loginError' => 'Login Failed.',
         ])->onlyInput('loginError');
     }
